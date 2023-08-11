d6aba905364bde5e78c6fc8955f932d9a7c5f9b3
JeffUwU <jeffersonbruno0210@gmail.com>
2023-08-11 15:03:13 -0300

    Tela-de-Login

---
+++ lib/components/cadastro.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 40, 
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget> [
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: Image.asset('assets/aplicativo-web.png'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: 'EMAIL',
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),  
+              ),
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'SENHA',
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ), 
+              ),
+              style: const TextStyle(fontSize: 20),
+            ),
+            Container(
+              height: 40, 
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                child:
+                const Text(
+                  'Recuperar senha',               
+                ),
+                onPressed: () {},
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color(0xFFF58524),
+                    Color(0xFFF92B7F),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+} 
